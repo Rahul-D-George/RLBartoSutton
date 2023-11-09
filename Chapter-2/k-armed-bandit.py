feat: replaced sample average with fixed step-size parameter

Chapter-2/k-armed-bandit.py
@@ -3,11 +3,12 @@ import numpy as np
 
 k = 20
 time_steps = 10000
-eps = 0.03
+eps = 0.1
+a = 0.1
 ps = np.random.normal(0.5, 0.2, k)
 totreward = 0
 
-qt = np.zeros((k, 2))
+qt = np.zeros(k)
 
 def evaluater(i):
     return np.random.normal(ps[i], 0.2)
@@ -15,16 +16,17 @@ def evaluater(i):
 
 for i in range(time_steps):
     if random() > eps:
-        action = np.argmax(qt[:, 0])
+        action = np.argmax(qt)
     else:
         action = np.random.randint(0, k)
     reward = evaluater(action)
-    m, n = qt[action]
-    qt[action][0] = (m*n + reward)/(n+1)
-    qt[action][1] += 1
+    # m, n = qt[action]
+    qt[action] = qt[action] + a * (reward - qt[action])
+    # qt[action][0] = (m*n + reward)/(n+1)
+    # qt[action][1] += 1
     totreward += reward
 
-p_error = np.sum(np.absolute((qt[:, 0] - ps)/ ps)) / k
+p_error = np.sum(np.absolute((qt - ps)/ ps)) / k
 
 
 print(f"Reward was {totreward} with epsilon of {eps}.\n"