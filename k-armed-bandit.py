73c37f1aaf1123f8ddcc9062860d3c396ab84297
Rahul George <rg922@gpu09.doc.ic.ac.uk>
2023-11-09 11:31:14 +0000

    feat: implemented simple version of k-armed-bandit, to observe how exploration vs exploitation impacts reward

--- k-armed-bandit.py
+++ k-armed-bandit.py
@@ -1,1 +1,27 @@
-print("Hello")
+from random import random
+import numpy as np
+
+k = 20
+time_steps = 10000
+eps = 0.3
+ps = np.random.normal(0.5, 0.2, k)
+totreward = 0
+
+qt = np.zeros((k, 2))
+
+def evaluater(i):
+    return np.random.normal(ps[i], 0.2)
+
+
+for i in range(time_steps):
+    if random() > eps:
+        action = np.argmax(qt[:, 0])
+    else:
+        action = np.random.randint(0, k)
+    reward = evaluater(action)
+    m, n = qt[action]
+    qt[action][0] = (m*n + reward)/(n+1)
+    qt[action][1] += 1
+    totreward += reward
+
+print(f"Reward was {totreward} with epsilon of {eps}.")
