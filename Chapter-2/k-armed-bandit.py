6f4aea88660aed831e22cab97cb63eb1d1c57ea4
Rahul George <rg922@gpu09.doc.ic.ac.uk>
2023-11-09 12:40:13 +0000

    feat: added random walk feature

--- Chapter-2/k-armed-bandit.py
+++ Chapter-2/k-armed-bandit.py
@@ -26,8 +26,17 @@ for i in range(time_steps):
     # qt[action][1] += 1
     totreward += reward
 
+    # Random Walk feature
+    ps_change = np.random.normal(0, 0.01, k)
+    ps = ps + ps_change
+
 p_error = np.sum(np.absolute((qt - ps)/ ps)) / k
 
 
 print(f"Reward was {totreward} with epsilon of {eps}.\n"
       f"Average % difference in true vs learned values: {round(p_error * 100, 3)}%")
+
+# It is apparent from the % differences obtained with/without using the random walks
+# that stationary bandits suffer immensely from this problem.
+
+print(qt, ps)
